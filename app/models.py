0eeff0a73f09348393d7c7aad948cc56aeccec6e
deepa28li <swaralipatil2000@gmail.com>
2024-03-04 15:10:22 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class School(models. Model):
+    scname=models.CharField(max_length=100)
+    scprincipal=models.CharField(max_length=100)
+    sclocation=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.scname
---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from app.models import *
+from app.serializers import *
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+# Create your views here.
+
+@api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
+def SchoolJsondata(request):
+    SOD=School.objects.all()
+    JOD=Schoomodelserializer(SOD,many=True)
+    jsondata=JOD.data
+    return Response(jsondata)
